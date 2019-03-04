f6aeffa78b7624feca04cd91cad6ab972834bcae
Jessica Gladstone <jcg321@comcast.net>
2019-03-04 18:52:26 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,16 +1,51 @@
 class LandmarksController < ApplicationController
 
+  set :views, Proc.new { File.join(root, "../views/") }
+  register Sinatra::Twitter::Bootstrap::Assets
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+
+    redirect to "landmark/#{@landmark.id}"
+  end
+
   get '/landmarks' do
     @landmarks = Landmark.all
 
     erb :'landmarks/index'
   end
 
-  get '/landmarks/new' do
-    erb :'landmarks/new'
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
   end
 
-  post '/landmarks' do
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed], 
+                    figure_id: params[:landmark][:figure_id])
+
+    redirect to "/landmarks/#{@landmark.id}"
+  end
+
+  delete '/landmarks/:id/delete' do
+    @landmark = Landmark.find(params[:id]).destroy
+
+    redirect to "/landmarks"
 
   end
+
+
 end
