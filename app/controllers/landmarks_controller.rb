72a52b5cad382fea6a8106a99d17d134a954f3f7
Jessica Gladstone <jcg321@comcast.net>
2019-03-04 19:04:06 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -30,10 +30,10 @@ class LandmarksController < ApplicationController
   end
 
   patch '/landmarks/:id' do
-    # binding.pry
+    binding.pry
     @landmark = Landmark.find(params[:id])
-    @landmark.update(name: params[:name], year_completed: params[:year_completed], 
-                    figure_id: params[:figure_id])
+    @landmark.update(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed], 
+                    figure_id: params[:landmark][:figure_id])
 
     redirect to "/landmarks/#{@landmark.id}"
   end
